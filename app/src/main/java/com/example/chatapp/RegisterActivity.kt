d4f0a431687c47d2b7c0b582db4da116ddf93719
AndresMarronP <60479310+AndresMarronP@users.noreply.github.com>
2020-05-06 13:01:11 +0200

    RegisterPartV2

--- app/src/main/java/com/example/chatapp/RegisterActivity.kt
+++ app/src/main/java/com/example/chatapp/RegisterActivity.kt
@@ -1,8 +1,8 @@
 package com.example.chatapp
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.chatapp.Data.User
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
@@ -14,13 +14,15 @@ class RegisterActivity : AppCompatActivity() {
 
 
 
+        //var user1= User(etUsername.text.toString(),etAge.text.toString().toInt(),etPassword.text.toString())
+
 
 
         btnSubmit.setOnClickListener() {
 
            /* val intent = Intent(this, DoRegister::class.java)
-            //var user1 = new user(etUsername.text,)
-            intent.putExtra("user", )
+            var user1= User(etUsername.text.toString(),etAge.text.toString().toInt(),etPassword.text.toString())
+            intent.putExtra("user", user1)
             startActivity(intent)*/
         }
 
