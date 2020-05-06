ed972500e9907bd65ac7893f24d350a0d9d15e92
AndresMarronP <60479310+AndresMarronP@users.noreply.github.com>
2020-05-06 13:16:26 +0200

    Parameters
    I modified the parameters

--- app/src/main/java/com/example/chatapp/Data/User.kt
+++ app/src/main/java/com/example/chatapp/Data/User.kt
@@ -1,13 +1,13 @@
 package com.example.chatapp.Data
 
 class User (emp_name: String, emp_age : Int, emp_password: String ) {
-    var Age: Int
+    var age: Int
     var username: String
     var password: String
 
     // initializer block
     init {
-        Age = emp_age
+        age = emp_age
         username = emp_name
         password= emp_password
 
