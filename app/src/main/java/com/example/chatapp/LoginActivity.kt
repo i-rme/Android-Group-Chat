8d0bb771d70470b3b7c7ff954ba7e7295f18bbe3
i-rme <i-rme@users.noreply.github.com>
2020-05-06 13:30:11 +0200

    Added login method

--- app/src/main/java/com/example/chatapp/LoginActivity.kt
+++ app/src/main/java/com/example/chatapp/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.example.chatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -11,13 +12,23 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         btnLogin.setOnClickListener {
-//            etUsername.text
-//            etPassword.text
+            login(etUsername.text.toString(), etPassword.text.toString())
+        }
+
+    }
+
+    fun login(username: String?, password: String?): Boolean {
+
+//        if(ContentProvider.login(User)){
+        if(true){
 //            val intent = Intent(this@MainActivity, SecondActivity::class.java)
 //            intent.putExtra("key", "")
 //            startActivity(intent)
+        }else{
+            val toast = Toast.makeText(applicationContext, "Incorrect username or password", Toast.LENGTH_SHORT)
+            toast.show()
         }
+        return true
     }
-
-
+    
 }
