ccb53c35af9f8c5dedf5bb0de8f1a50d14870234
i-rme <i-rme@users.noreply.github.com>
2020-05-08 02:12:50 +0200

    Register method working, trying to fix gitignore
    Register method working, trying to fix gitignore

--- app/src/main/java/com/example/chatapp/RegisterActivity.kt
+++ app/src/main/java/com/example/chatapp/RegisterActivity.kt
@@ -2,31 +2,49 @@ package com.example.chatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.chatapp.data.User
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
+
+    lateinit var database: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-
-
-
-        //var user1= User(etUsername.text.toString(),etAge.text.toString().toInt(),etPassword.text.toString())
-
-
-
         btnSubmit.setOnClickListener() {
 
            /* val intent = Intent(this, DoRegister::class.java)
-            var user1= User(etUsername.text.toString(),etAge.text.toString().toInt(),etPassword.text.toString())
             intent.putExtra("user", user1)
             startActivity(intent)*/
+            register(etUsername.text.toString(), etPassword.text.toString(), etAge.text.toString().toInt())
+
         }
 
+    }
 
+    fun register(username: String, password: String, age: Int): Boolean {
 
+        if(true){
+            database = FirebaseDatabase.getInstance().reference
+
+            var user = User(username, password, age)
+            database.child("users").child(user.username!!).setValue(user)
+
+            val toast = Toast.makeText(applicationContext, "New user registered", Toast.LENGTH_SHORT)
+            toast.show()
+
+            return true
+        }else{
+            val toast = Toast.makeText(applicationContext, "Incorrect username or password", Toast.LENGTH_SHORT)
+            toast.show()
+
+            return false
+        }
 
     }
 }
--- app/src/main/java/com/example/chatapp/data/User.kt
+++ app/src/main/java/com/example/chatapp/data/User.kt
@@ -1,4 +1,4 @@
 package com.example.chatapp.data
 
-data class User (val username: String, val password: String, val age : Int) {
+data class User (val username: String?, val password: String?, val age : Int?) {
 }
