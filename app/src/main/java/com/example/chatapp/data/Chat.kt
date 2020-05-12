b653975988e84bfa9732b1daa9bc329ba5c08666
i-rme <i-rme@users.noreply.github.com>
2020-05-12 13:57:14 +0200

    Added Push method, changed data classes
    Added Push method, changed data classes

--- app/src/main/java/com/example/chatapp/data/Chat.kt
+++ app/src/main/java/com/example/chatapp/data/Chat.kt
@@ -1,5 +1,5 @@
 package com.example.chatapp.data
 
-data class Chat (val id: Int, val users: ArrayList<User>, val messages: ArrayList<Message> ) {
+data class Chat (val id: String, val users: ArrayList<User>, val messages: ArrayList<Message> ) {
 
 }
