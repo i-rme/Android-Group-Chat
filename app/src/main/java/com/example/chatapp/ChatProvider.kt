89e39e3d6eac84976b5ff775e6eccfe0ba612924
i-rme <i-rme@users.noreply.github.com>
2020-05-12 12:24:35 +0200

    postMessage & fixed gradle
    postMessage & fixed gradle

--- app/src/main/java/com/example/chatapp/ChatProvider.kt
+++ app/src/main/java/com/example/chatapp/ChatProvider.kt
@@ -1,7 +1,23 @@
 package com.example.chatapp
 
-class ChatProvider {
+import com.example.chatapp.data.Message
+import com.google.firebase.database.FirebaseDatabase
 
+object ChatProvider {
 
+    fun postMessage(chatId: Int, message: Message) {
+
+        val database = FirebaseDatabase.getInstance().reference
+        database.child("chats").child(chatId.toString()).child(message.id.toString()).setValue(message)
+
+        /*
+            USAGE
+            
+            var testUser = User("TestUsername", "password", 43)
+            var testMessage = Message(1, testUser, "This is my first message")
+            ChatProvider.postMessage(1, testMessage)
+
+         */
+    }
 
 }
