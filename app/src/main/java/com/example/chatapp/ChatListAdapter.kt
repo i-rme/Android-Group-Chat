1b12fd9ac7a325faa85ce712f43b23a134ebb990
Adrian <andrei.grecu100@gmail.com>
2020-05-20 13:14:30 +0200

    randomized images

--- app/src/main/java/com/example/chatapp/ChatListAdapter.kt
+++ app/src/main/java/com/example/chatapp/ChatListAdapter.kt
@@ -1,15 +1,19 @@
 package com.example.chatapp
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.recyclerview.widget.RecyclerView
 import com.example.chatapp.data.Chat
 import kotlinx.android.synthetic.main.list_chat_item.view.*
+import java.util.*
+import kotlin.collections.ArrayList
+
+private val randomGenerator = Random()
 
 
 class ChatListAdapter(private val context: Context, private val chatList :MutableList<Chat>)  : BaseAdapter() {
@@ -20,11 +24,16 @@ class ChatListAdapter(private val context: Context, private val chatList :Mutabl
 
         val rowView = convertView ?: LayoutInflater.from(context).inflate(R.layout.list_chat_item, parent, false)
 
+        var imageList = ArrayList<Int>()
+        imageList = initImageList(imageList)
+        val index = randomGenerator.nextInt(imageList.size)
+
         val tvChatName = rowView.tvChatNameList as TextView
         val avatarChat = rowView.ivAvatarChat as ImageView
 
         tvChatName.text = chat.chatName
-        avatarChat.setImageResource(R.mipmap.ic_launcher)
+        Log.v("IMAGE SELECTION: ", "$index")
+        avatarChat.setImageResource(imageList[index])
 
         return rowView
     }
@@ -41,5 +50,11 @@ class ChatListAdapter(private val context: Context, private val chatList :Mutabl
         return chatList.size
     }
 
+    fun initImageList(imageList: ArrayList<Int>) : ArrayList<Int>{
+        imageList.add(R.mipmap.ic_launcher)
+        imageList.add(R.drawable.ic_launcher_foreground)
+        imageList.add(R.drawable.ic_people_white_24dp)
+        return imageList
+    }
 
 }
